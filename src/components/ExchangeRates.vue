ba9248a3ee8036262264198d12808d138088bcad
David <raymondinf23@gmail.com>
2021-09-30 13:03:43 -0400

    Create Exchange Rates component

---
+++ src/components/ExchangeRates.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="exchanges-container">
+      <ol v-if="currencyRates.length">
+          <li 
+            v-for="(currencyRate, index) in currencyRates"
+            v-show="currentCurrency !== currencyRate.name"
+            :key="index"
+            >
+                <span>{{ currencyRate.name }} : {{ currencyRate.value }}</span>
+            </li>
+      </ol>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentCurrency: {
+            type: String,
+            required: true
+        },
+        currencyRates: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+.exchanges-container {
+    display: flex;
+    justify-content: center;
+}
+</style>
