bc6b5f964771c3243b5944c0c9f5977cc4608894
David <raymondinf23@gmail.com>
2021-09-30 13:24:06 -0400

    Finish App

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- EUR, USD, BRL, PEN, and ARS -->
+  <section class="container">
+    <select v-model="currentCurrency">
+      <option 
+        v-for="(currency, index) in currencies"
+        :key="index"
+      >{{ currency }}</option>
+    </select>
+
+    <ExchangeRates
+      :isLoading="isLoading"
+      :currentCurrency="currentCurrency" 
+      :currencyRates="currencyRates" 
+    />
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import ExchangeRates from './components/ExchangeRates.vue'
 
 export default {
   name: 'App',
+  created () {
+    this.fetchCurrencyRates(this.currentCurrency)
+  },
+  data () {
+    return {
+      currencies: ['USD', 'EUR', 'BRL', 'PEN', 'ARS'],
+      currentCurrency: 'USD',
+      currencyRates: [],
+      isLoading: true,
+    }
+  },
+  watch: {
+    currentCurrency(newCurrency) {
+      this.fetchCurrencyRates(newCurrency)
+    }
+  },
+  methods: {
+    fetchCurrencyRates (currency) {
+      this.isLoading = true;
+
+      const apiURL = `https://v6.exchangerate-api.com/v6/72179b357c84d84fcb2c1f3d/latest/${ currency }`;
+
+      axios.get(apiURL)
+        .then(({ data }) => {
+          const filteredCurrencies = this.currencies.filter(currency => currency in data.conversion_rates)
+                                                    .map(currency => ({
+                                                      name: currency,
+                                                      value: data.conversion_rates[currency]
+                                                    }));
+          this.currencyRates = filteredCurrencies
+          this.isLoading = false;
+        })
+        .catch(() => {
+          this.isLoading = false;
+          console.warn("Something wrong happened, please try later...")
+        })
+      
+    }
+  },
   components: {
-    HelloWorld
+    ExchangeRates
   }
 }
 </script>
 
 <style>
-#app {
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
