3df98192b17b8f654a9b9430fc739bb85f732a97
David <raymondinf23@gmail.com>
2021-09-30 13:23:13 -0400

    Create exchange rate component

---
+++ src/components/ExchangeRate.vue
@@ -0,0 +1,24 @@
+<template>
+  <li v-if="currentCurrency !== currencyRate.name">
+      {{ currencyRate.name }} : {{ currencyRate.value }}
+  </li>
+</template>
+
+<script>
+export default {
+    props: {
+        currentCurrency: {
+            type: String,
+            required: true,
+        },
+        currencyRate: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
